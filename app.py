af369bfa96da2769aa2c734ac03e6066f2937650
pgelvin <patrick_gelvin@intuit.com>
2022-05-30 03:09:42 +0000

    With dropdown code

--- app.py
+++ app.py
@@ -63,6 +63,47 @@ app.layout = html.Div(children=[
     ]
 )
 
+########## Set up the dropdown box
+import plotly.graph_objects as go
+import numpy as np
+  
+# creating random data through randomint
+# function of numpy.random
+np.random.seed(42)
+  
+random_x = np.random.randint(1, 101, 100)
+random_y = np.random.randint(1, 101, 100)
+  
+plot = go.Figure(data=[go.Scatter(
+    x=random_x,
+    y=random_y,
+    mode='markers',)
+])
+  
+# Add dropdown
+plot.update_layout(
+    updatemenus=[
+        dict(
+            buttons=list([
+                dict(
+                    args=["type", "scatter"],
+                    label="Scatter Plot",
+                    method="restyle"
+                ),
+                dict(
+                    args=["type", "bar"],
+                    label="Bar Chart",
+                    method="restyle"
+                )
+            ]),
+            direction="down",
+        ),
+    ]
+)
+  
+plot.show()
+########## End of the dropdown box code
+
 ############ Deploy
 if __name__ == '__main__':
     app.run_server()
