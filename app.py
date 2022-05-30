91e742f05b713ab35fa70a8c065d6dc6b7b99727
pgelvin <patrick_gelvin@intuit.com>
2022-05-30 03:18:38 +0000

    with dropdown code 2

--- app.py
+++ app.py
@@ -23,46 +23,6 @@ sourceurl = 'https://plot.ly/python/choropleth-maps/'
 # githublink = 'https://github.com/austinlasseter/dash-map-usa-agriculture' # this is the original repo
 githublink = 'https://github.com/pgelvin/301-old-mcdonald.git'
 
-########## Set up the chart
-
-import pandas as pd
-df = pd.read_csv('assets/usa-2011-agriculture.csv')
-
-fig = go.Figure(data=go.Choropleth(
-    locations=df['code'], # Spatial coordinates
-    z = df[mycolumn].astype(float), # Data to be color-coded
-    locationmode = 'USA-states', # set of locations match entries in `locations`
-    colorscale = mycolorscale,
-    colorbar_title = mycolorbartitle,
-))
-
-fig.update_layout(
-    title_text = mygraphtitle,
-    geo_scope='usa',
-    width=1200,
-    height=800
-)
-
-########### Initiate the app
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
-app.title=tabtitle
-
-########### Set up the layout
-
-app.layout = html.Div(children=[
-    html.H1(myheading1),
-    dcc.Graph(
-        id='figure-1',
-        figure=fig
-    ),
-    html.A('Code on Github', href=githublink),
-    html.Br(),
-    html.A("Data Source", href=sourceurl),
-    ]
-)
-
 ########## Set up the dropdown box
 import plotly.graph_objects as go
 import numpy as np
@@ -103,6 +63,45 @@ plot.update_layout(
   
 plot.show()
 ########## End of the dropdown box code
+
+########## Set up the chart
+import pandas as pd
+df = pd.read_csv('assets/usa-2011-agriculture.csv')
+
+fig = go.Figure(data=go.Choropleth(
+    locations=df['code'], # Spatial coordinates
+    z = df[mycolumn].astype(float), # Data to be color-coded
+    locationmode = 'USA-states', # set of locations match entries in `locations`
+    colorscale = mycolorscale,
+    colorbar_title = mycolorbartitle,
+))
+
+fig.update_layout(
+    title_text = mygraphtitle,
+    geo_scope='usa',
+    width=1200,
+    height=800
+)
+
+########### Initiate the app
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+app.title=tabtitle
+
+########### Set up the layout
+
+app.layout = html.Div(children=[
+    html.H1(myheading1),
+    dcc.Graph(
+        id='figure-1',
+        figure=fig
+    ),
+    html.A('Code on Github', href=githublink),
+    html.Br(),
+    html.A("Data Source", href=sourceurl),
+    ]
+)
 
 ############ Deploy
 if __name__ == '__main__':
